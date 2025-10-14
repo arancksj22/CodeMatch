5268c3e610c1edd93744601496c34b31046df32d
arancksj22 <arancksj@gmail.com>
2025-10-14 22:18:29 +0530

    deleteion

--- tester.py
+++ tester.py
@@ -10,15 +10,3 @@ kmeans = KMeans(n_clusters=3, random_state=42)
 kmeans.fit(data)
 
 data['Cluster'] = kmeans.labels_
-
-print("Cluster Centers:\n", kmeans.cluster_centers_)
-
-plt.figure(figsize=(8, 5))
-plt.scatter(data.iloc[:, 0], data.iloc[:, 1], c=data['Cluster'], cmap='viridis', s=50)
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1],
-            c='red', marker='X', s=200, label='Centroids')
-plt.title('K-Means Clustering on IRIS Dataset')
-plt.xlabel('Sepal length (cm)')
-plt.ylabel('Sepal width (cm)')
-plt.legend()
-plt.show()
